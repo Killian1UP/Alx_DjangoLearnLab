11a2f089ec11335faf04a23ddef702e29f6b6617
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-11-09 23:17:58 +0200

    Created a set of models that shows the use of ForeignKey, ManyToMany and OneToOne relationships.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        books = author.books.all()  # Access books using the related_name defined in ForeignKey
+        books = Book.objects.filter(author=author)  # Access books using the related_name defined in ForeignKey
         return books
     except Author.DoesNotExist:
         return f"No author found with the name {author_name}"
