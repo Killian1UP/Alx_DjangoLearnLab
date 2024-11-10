571716fe7e4b6255e5d3bb1f9f891e4ec28f833b
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-11-10 22:53:08 +0200

    Set up user login, logout, and registration functionalities using Django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,3 +5,32 @@ urlpatterns = [
     path('list/', list_books, name='list'),
     path('library/', LibraryDetailView.as_view, name='library'),
 ]
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
+
+from .views import RegisterView
+from django.urls import path
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+]
+
+from django.views.generic import TemplateView
+from django.urls import path, include
+
+urlpatterns = [
+    ...,
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile/',
+             TemplateView.as_view(template_name='accounts/profile.html'),
+             name='profile'),
+    path("signup/", RegisterView.as_view(), name="templates/registration/register"),
+        ...
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -18,3 +18,12 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.objects.books.all()
         return context
+    
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/register.html"
