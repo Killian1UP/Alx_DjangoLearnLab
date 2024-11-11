eb8b540ce06ccadaf4bcd58e0afe8827c7863973
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-11-11 02:56:39 +0200

    Implemented role-based access control within a Django application to manage different user roles and permissions effectively

--- django-models/LibraryProject/relationship_app/librarian_view.py
+++ django-models/LibraryProject/relationship_app/librarian_view.py
@@ -1,9 +1,9 @@
+from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponse
 
 def is_librarian(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return user.userprofile.role == 'Librarian'
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return HttpResponse("Welcome, Librarian!")
+    return render(request, 'librarian_template.html')
--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -1,9 +1,9 @@
+from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponse
 
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return HttpResponse("Welcome, Admin!")
+    return render(request, 'admin_template.html')
