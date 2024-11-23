6a6ffc5a9d80b5334ccb6aa55b3fbcfc84a85977
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-11-23 17:01:10 +0200

    Expanded my API functionality by using Django REST Frameworks ViewSets and Routers to implement CRUD operations on the Book model.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializers
 # Create your views here.
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
