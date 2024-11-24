dd5a6602c7f30237e6155d8ba56e804ccbfc85dc
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-11-24 18:26:20 +0200

    Secured my API endpoints by implementing various authentication schemes and permission settings in Django REST Framework.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token # Handles the token retrieval for verifying the user's username and password
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -10,4 +11,5 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),
+    path('token/', obtain_auth_token, name='obtain-auth-token'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializers
+from rest_framework.permissions import IsAuthenticated # Import the permission
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -10,3 +11,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
+    permission_classes = [IsAuthenticated] #Restrict access to authenticated users
