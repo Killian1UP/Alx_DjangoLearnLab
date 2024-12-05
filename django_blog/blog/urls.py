3e774cd66a2c076a380c903c3a68c74291c83c17
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-12-06 01:51:23 +0200

    Developed and intergrated features in your Django blog project that allow users to create, read, update, and delete (CRUD) blog posts. This will enable authors to manage their content dynamically and viewers to browse blog entries effectively

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     # CRUD Operations Views
     path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
